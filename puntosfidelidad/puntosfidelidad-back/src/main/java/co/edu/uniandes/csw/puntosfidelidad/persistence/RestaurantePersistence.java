bd4c708c6c72718e5afff3b9e338b9f5d57ef480
Sebastian Cespedes Otalora <s.cespedes10@sis.virtual.uniandes.edu.co>
2017-09-11 08:47:17 -0500

    Entidades y persistencia creadas

---
+++ puntosfidelidad/puntosfidelidad-back/src/main/java/co/edu/uniandes/csw/puntosfidelidad/persistence/RestaurantePersistence.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.puntosfidelidad.persistence;
+
+import co.edu.uniandes.csw.puntosfidelidad.entities.AdministradorEntity;
+import co.edu.uniandes.csw.puntosfidelidad.entities.RestauranteEntity;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
+/**
+ *
+ * @author s.cespedes10
+ */
+@Stateless
+public class RestaurantePersistence {
+    
+     @PersistenceContext(unitName = "puntosfidelidadPU")
+    protected EntityManager em;
+    
+    
+    public RestauranteEntity find(String usuario) {
+        
+        return em.find(RestauranteEntity.class, usuario);
+    }
+    
+    public RestauranteEntity findByName(String usuario) {
+        
+        TypedQuery<RestauranteEntity> q
+                = em.createQuery("select u from RestauranteEntity u where u.usuario = :usuario", RestauranteEntity.class);
+        q = q.setParameter("usuario", usuario);
+        return q.getSingleResult();
+    }
+    
+    
+    public List<RestauranteEntity> findAll() {
+        
+        Query q = em.createQuery("select u from RestauranteEntity u");
+        return q.getResultList();
+    }
+
+    public RestauranteEntity create(RestauranteEntity entity) {
+        
+        em.persist(entity);
+        return entity;
+    }
+
+    public RestauranteEntity update(RestauranteEntity entity) {
+        
+        return em.merge(entity);
+    }
+
+    public void delete(Long id) {
+       
+        RestauranteEntity entity = em.find(RestauranteEntity.class, id);
+        em.remove(entity);
+    }
+    
+    
+    
+}
