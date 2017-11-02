3b559f9033b3b298dc9d79e6908756cccdaa98df
ALVARO ALFONSO YEPES BONILLA <aa.yepes@sis.virtual.uniandes.edu.co>
2017-11-02 16:29:26 -0500

    todo producto funcionando

--- puntosfidelidad-web/src/main/webapp/src/modules/producto/update/productos.update.ctrl.js
+++ puntosfidelidad-web/src/main/webapp/src/modules/producto/update/productos.update.ctrl.js
@@ -2,22 +2,16 @@
         function (ng) {
             var mod = ng.module("productoModule");
             mod.constant("productosContext", "api/productos");
-            mod.constant("booksContext", "api/books");
-            mod.controller('productosUpdateCtrl', ['$scope', '$http', 'productosContext', '$state', 'booksContext', '$rootScope', '$filter',
-                function ($scope, $http, productosContext, $state, booksContext, $rootScope, $filter) {
+            mod.controller('productosUpdateCtrl', ['$scope', '$http', 'productosContext', '$state', '$rootScope', '$filter',
+                function ($scope, $http, productosContext, $state, $rootScope, $filter) {
                     $rootScope.edit = true;
 
                     var idProducto = $state.params.productoId;
-
-                    // Este arreglo guardara los ids de los books asociados y por asociar al autor.
-                    var idsBook = [];
-
-                    // Este arreglo mostrará los books una vez esten filtrados visualmente por lo que el autor ya tiene asociado.
-                    $scope.allBooksShow = [];
-
+                    
                     //Consulto el autor a editar.
                     $http.get(productosContext + '/' + idProducto).then(function (response) {
                         var producto = response.data;
+                        $scope.productoId = producto.id;
                         $scope.productoName = producto.nombre;
                         $scope.productoValorPuntos = producto.valorPuntos;
                         $scope.productoValorDinero = producto.valorDinero;                                   
@@ -35,34 +29,16 @@
                         ev.dataTransfer.setData("text", ev.target.id);
                     };
 
-                    $scope.dropAdd = function (ev) {
-                        ev.preventDefault();
-                        var data = ev.dataTransfer.getData("text");
-                        ev.target.appendChild(document.getElementById(data));
-                        //Cuando un book se añade al autor, se almacena su id en el array idsBook
-                        idsBook.push("" + data);
-                    };
-
-                    $scope.dropDelete = function (ev) {
-                        ev.preventDefault();
-                        var data = ev.dataTransfer.getData("text");
-                        ev.target.appendChild(document.getElementById(data));
-                        //Para remover el book que no se va asociar, por eso se usa el splice que quita el id del book en el array idsBook
-                        var index = idsBook.indexOf(data);
-                        if (index > -1) {
-                            idsBook.splice(index, 1);
-                        }
-                    };
 
                     $scope.createProducto = function () {
                         /*Se llama a la función newBooks() para buscar cada uno de los ids de los books
                          en el array que tiene todos los books y así saber como queda la lista final de los books asociados al autor.
                          */
-                        $scope.newBooks();
                         $http.put(productosContext + "/" + idProducto, {
-                            name: $scope.productoName,
-                            productoValorPuntos: $scope.productoValorPuntos,
-                            productoValorDinero: $scope.productoValorDinero,
+                            id: $scope.productoId,
+                            nombre: $scope.productoName,
+                            valorPuntos: $scope.productoValorPuntos,
+                            valorDinero: $scope.productoValorDinero,
                         }).then(function (response) {
                 
                             //Author created successfully
