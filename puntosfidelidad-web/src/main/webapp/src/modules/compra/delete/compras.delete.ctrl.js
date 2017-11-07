bbfbc495f1cb597987a83cb0ea66107e6a8fd740
ALVARO ALFONSO YEPES BONILLA <aa.yepes@sis.virtual.uniandes.edu.co>
2017-11-07 17:50:31 -0500

    delete

--- puntosfidelidad-web/src/main/webapp/src/modules/compra/delete/compras.delete.ctrl.js
+++ puntosfidelidad-web/src/main/webapp/src/modules/compra/delete/compras.delete.ctrl.js
@@ -1,11 +1,11 @@
 (function (ng) {
     var mod = ng.module("comprasModule");
-    mod.constant("comprasContext", "api/productos");
+    mod.constant("comprasContext", "api/compras");
     mod.controller('comprasDeleteCtrl', ['$scope', '$http', 'comprasContext', '$state',
         function ($scope, $http, comprasContext, $state) {
-            var idCompra = $state.params.idCompra;
+            var idCompra = $state.params.compraId;
             $scope.deleteCompra= function () {
-                $http.delete(ProdContext + '/' + idCompra, {}).then(function (response) {
+                $http.delete(comprasContext + '/' + idCompra, {}).then(function (response) {
                     $state.go('comprasList', {idCompra: response.data.id}, {reload: true});
                 });
             };
