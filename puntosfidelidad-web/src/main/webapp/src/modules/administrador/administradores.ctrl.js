2c1fca3034d13bb98e227ecf02763ec3b0b60231
Sebastian Cespedes Otalora <s.cespedes10@sis.virtual.uniandes.edu.co>
2017-11-08 02:46:02 -0500

    Borrar Admin

--- puntosfidelidad-web/src/main/webapp/src/modules/administrador/administradores.ctrl.js
+++ puntosfidelidad-web/src/main/webapp/src/modules/administrador/administradores.ctrl.js
@@ -7,6 +7,37 @@
                         $scope.administradoresLista = response.data;
             });           
             
-        }]);
+        
+    
+    $scope.deleteAdministrador = function(administrador)
+            {
+                quiereBorrar = confirm("¿Seguro desea borrar el Administrador?");
 
+                if(quiereBorrar)
+                {
+                    usuario = administrador.usuario;
+                    
+
+                    $http.delete('http://localhost:8080/puntosfidelidad-web/api/administradores/' + usuario).then(
+                        function todoBien(response)
+                        {
+                            index = $scope.elements.indexOf(administrador);
+                            if(index > -1)
+                            {
+                                $scope.elements.splice(index, 1);
+                                console.log("todo bien!");
+                            }
+                            else
+                            {
+                                console.log("Error con el index!")
+                            }
+                        },
+                        function todoMal(error)
+                        {
+                            console.log(error);
+                        }
+                    );
+                }
+            }
+}]);
 })(window.angular);
