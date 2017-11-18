0ff43e5b3de2266ac407b776b495fa79feb4716c
Camilo Sánchez Salamanca <ca.sanchez38@uniandes.edu.co>
2017-11-18 13:56:59 -0500

    Basura

--- puntosfidelidad-web/src/main/webapp/src/modules/LogIn/login.ctrl.js
+++ puntosfidelidad-web/src/main/webapp/src/modules/LogIn/login.ctrl.js
@@ -4,8 +4,7 @@
     
         mod.controller("loginCtrl", ['$scope', '$state', '$stateParams', '$http', 'loginContext', function ($scope, $state, $stateParams, $http, context) {
             
-            //Código del controlador. Arriba se pueden reemplazar las dependencias con las reuqeridas
-    
+            //Código del controlador. Arriba se pueden reemplazar las dependencias con las reuqeridas    
             }]);
     })(window.angular);
     
