b1ab5e0936d53de9c9591468d622c797abf6239a
FAMILIAVANEGAS\LauraValeria <lauvalevanegas@gmail.com>
2017-11-18 18:33:47 -0500

    LogIn html

--- puntosfidelidad-web/src/main/webapp/src/modules/LogIn/login.ctrl.js
+++ puntosfidelidad-web/src/main/webapp/src/modules/LogIn/login.ctrl.js
@@ -1,10 +1,9 @@
 (function (ng) {
-    
-        var mod = ng.module("loginModule");
-    
-        mod.controller("loginCtrl", ['$scope', '$state', '$stateParams', '$http', 'loginContext', function ($scope, $state, $stateParams, $http, context) {
-            
-            //Código del controlador. Arriba se pueden reemplazar las dependencias con las reuqeridas    
-            }]);
-    })(window.angular);
+
+    var mod = ng.module("loginModule");
+
+    mod.controller("loginCtrl", ['$scope', '$state', '$stateParams', '$http', 'loginContext', 'document', function (document, $state) {
+           
+        }]);
+})(window.angular);
     
