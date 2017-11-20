9e8165deb2e4f56c68da04643c00f5446039f00d
FAMILIAVANEGAS\LauraValeria <lauvalevanegas@gmail.com>
2017-11-19 19:16:44 -0500

    login Administrador

--- puntosfidelidad-web/src/main/webapp/src/modules/LogIn/login.ctrl.js
+++ puntosfidelidad-web/src/main/webapp/src/modules/LogIn/login.ctrl.js
@@ -5,30 +5,54 @@
 
             $scope.user = {};
             $scope.data = {};
-            var clienteLogin = [];
+            var usuarioLogin = [];
 
             $scope.autenticar = function () {
                 if ($scope.data.rol === "cliente") {
 
                     $http.get("api/clientes/" + $scope.data.usuario)
                             .then(function (response) {
-                                clienteLogin = response.data;
+                                usuarioLogin = response.data;
 
-                                if (clienteLogin.contrasena === $scope.data.password) {
-                                    sessionStorage.token = clienteLogin.token;
-                                    sessionStorage.setItem("usuario", clienteLogin.usuario);
-                                    sessionStorage.setItem("nombre", clienteLogin.nombre);
+                                if (usuarioLogin.contrasena === $scope.data.password) {
+                                    sessionStorage.token = usuarioLogin.token;
+                                    sessionStorage.setItem("usuario", usuarioLogin.usuario);
+                                    sessionStorage.setItem("nombre", usuarioLogin.nombre);
                                     sessionStorage.setItem("rol", "cliente");
-                                    $rootScope.currentUser = clienteLogin.usuario;
+                                    $rootScope.currentUser = usuarioLogin.usuario;
                                     $scope.hayError = false;
-                                    $state.go('clienteDetail', {'clienteUsuario': clienteLogin.usuario}, {reload: true});
+                                    $state.go('clienteDetail', {'clienteUsuario': usuarioLogin.usuario}, {reload: true});
                                 } else {
                                     $scope.hayError = true;
                                     $scope.errortxt= "  Contraseña incorrecta";
                                 }
                             }, function() {
                                  $scope.hayError = true;
-                                 $scope.errortxt= " Parece que el usuario no existe";
+                                 $scope.errortxt= " Parece que el cliente no existe";
+                            });
+                }
+                
+                if ($scope.data.rol === "administrador") {
+
+                    $http.get("api/administradores/" + $scope.data.usuario)
+                            .then(function (response) {
+                                usuarioLogin = response.data;
+
+                                if (usuarioLogin.contrasena === $scope.data.password) {
+                                    sessionStorage.token = usuarioLogin.token;
+                                    sessionStorage.setItem("usuario", usuarioLogin.usuario);
+                                    sessionStorage.setItem("nombre", usuarioLogin.nombre);
+                                    sessionStorage.setItem("rol", "administrador");
+                                    $rootScope.currentUser = usuarioLogin.usuario;
+                                    $scope.hayError = false;
+                                    $state.go('administradorDetail', {'administradorUsuario': usuarioLogin.usuario}, {reload: true});
+                                } else {
+                                    $scope.hayError = true;
+                                    $scope.errortxt= "  Contraseña incorrecta";
+                                }
+                            }, function() {
+                                 $scope.hayError = true;
+                                 $scope.errortxt= " Parece que el administrador no existe";
                             });
                 }
 
