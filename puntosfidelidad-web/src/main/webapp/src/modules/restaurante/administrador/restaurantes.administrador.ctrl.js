5aa3865861ec2ef499881e01b9cf9e1d03f9e8a2
Sebastian Cespedes Otalora <s.cespedes10@sis.virtual.uniandes.edu.co>
2017-11-24 11:45:48 -0500

    Cambios front Admin Restaurante

--- puntosfidelidad-web/src/main/webapp/src/modules/restaurante/administrador/restaurantes.administrador.ctrl.js
+++ puntosfidelidad-web/src/main/webapp/src/modules/restaurante/administrador/restaurantes.administrador.ctrl.js
@@ -5,7 +5,49 @@
             $http.get("http://localhost:8080/puntosfidelidad-web/api/administradores/"+$state.params.administradorUsuario+"/restaurantes")
                     .then(function (response) {
                         $scope.elementosRes = response.data;
-            });
+                
+                 });
+                
+                 $scope.postRestaurante = function(TP)
+            {
+                restaurante = {};
+                restaurante.nit = TP.nit;
+                restaurante.nombre = TP.nombre;
+                restaurante.tipoComida = TP.tipoComida;
+                
+                cliente = null;
+
+                $http.get('http://localhost:8080/puntosfidelidad-web/api/clientes/' + TP.usuarioCliente).then(
+                    function(response)
+                    {
+                        resp = response.data;
+                        cliente = {};
+                        cliente.usuario = resp.usuario;
+
+                        restaurante.cliente = cliente;
+                        $scope.elements = $scope.elements.concat(restaurante);
+                        $scope.crear = false;
+                        $http.post('http://localhost:8080/puntosfidelidad-web/api/clientes/' + TP.usuarioCliente + '/tarjetasPuntos/' + restaurante.id).then(
+                            function(response)
+                            {
+                                alert('Tarjeta puntos creada exitosamente!');
+                            },
+                            function(error)
+                            {
+                                //console.log(error); comentado para reducir deuda técnica
+                                //alert('Error creando la tarjeta de puntos: ' + error.message);
+                            }
+                        )
+                    },
+                    function(error)
+                    {
+                        alert("El cliente no existe!");
+                    }
+                );                
+            }
+                
+                
+           
         }]);
 
 })(window.angular);
