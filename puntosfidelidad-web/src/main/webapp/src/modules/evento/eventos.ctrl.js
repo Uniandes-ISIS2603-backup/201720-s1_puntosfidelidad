482c1291fe9d6d359f76899a9e786d2b4890707b
Camilo Sánchez Salamanca <ca.sanchez38@uniandes.edu.co>
2017-11-28 16:15:29 -0500

    Carrusel en eventos agregados y resize del video en index

--- puntosfidelidad-web/src/main/webapp/src/modules/evento/eventos.ctrl.js
+++ puntosfidelidad-web/src/main/webapp/src/modules/evento/eventos.ctrl.js
@@ -5,6 +5,9 @@
             $http.get("http://localhost:8080/puntosfidelidad-web/api/eventos")
                     .then(function (response) {
                         $scope.eventos = response.data;
+                        $scope.event1 = $scope.eventos[0];
+                        $scope.event2 = $scope.eventos[1];
+                        $scope.event3 = $scope.eventos[2];
             });
         }]);
 })(window.angular);
