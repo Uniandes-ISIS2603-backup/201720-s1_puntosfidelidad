21ce70d986839da10c38ed717ec85f399b8488a0
Julián Manrique <ja.manrique@uniandes.edu.co>
2017-11-30 14:38:15 -0500

    Revert "Arreglos en comentarios de restaurante e inicios de mapa"
    This reverts commit c74e05ec51ac84e34b8c8df5133ed55c75d31621.

--- puntosfidelidad-web/src/main/webapp/src/modules/restaurante/administrador/restaurantes.administrador.mod.js
+++ puntosfidelidad-web/src/main/webapp/src/modules/restaurante/administrador/restaurantes.administrador.mod.js
@@ -116,22 +116,6 @@
                         templateUrl: basePath + 'restaurantes.productos.html'
                     }
                 }
-            }).state('restauranteAdministradorComentario', {
-                url: '/comentario',
-                parent: 'restauranteAdministradorDetail',
-                data: {
-                    requireLogin: true
-                },
-                param: {
-                    restauranteNit: null
-                },
-                views: {
-                    'infoRes': {
-                        controller: 'comentariosNuevosCtrl',
-                        controllerAs: 'ctrl',
-                        templateUrl: 'src/modules/comentario/comentarios.list.param.html'
-                    }
-                }
             });
         }]);
 
