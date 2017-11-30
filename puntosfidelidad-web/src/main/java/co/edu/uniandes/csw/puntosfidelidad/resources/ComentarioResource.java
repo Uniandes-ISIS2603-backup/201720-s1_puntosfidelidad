41803060f0d6aaa37c8e80eef1e801956ce3380a
ALVARO ALFONSO YEPES BONILLA <aa.yepes@sis.virtual.uniandes.edu.co>
2017-11-29 21:15:27 -0500

    filtro comentarios

--- puntosfidelidad-web/src/main/java/co/edu/uniandes/csw/puntosfidelidad/resources/ComentarioResource.java
+++ puntosfidelidad-web/src/main/java/co/edu/uniandes/csw/puntosfidelidad/resources/ComentarioResource.java
@@ -8,7 +8,10 @@ package co.edu.uniandes.csw.puntosfidelidad.resources;
 import co.edu.uniandes.csw.puntosfidelidad.dtos.ComentarioDTO;
 import co.edu.uniandes.csw.puntosfidelidad.dtos.ComentarioDetailDTO;
 import co.edu.uniandes.csw.puntosfidelidad.ejb.ComentarioLogic;
+import co.edu.uniandes.csw.puntosfidelidad.ejb.RestauranteLogic;
 import co.edu.uniandes.csw.puntosfidelidad.entities.ComentarioEntity;
+import co.edu.uniandes.csw.puntosfidelidad.entities.RestauranteEntity;
+import co.edu.uniandes.csw.puntosfidelidad.entities.SucursalEntity;
 import co.edu.uniandes.csw.puntosfidelidad.exceptions.BusinessLogicException;
 import java.util.ArrayList;
 import java.util.List;
@@ -24,85 +27,93 @@ import javax.ws.rs.Produces;
 @Path("comentarios")
 @Produces("application/json")
 public class ComentarioResource {
-    
+
     @Inject
     ComentarioLogic logic;
     
+    @Inject 
+    RestauranteLogic restLogic;
+
     /**
-     *  Obtiene una lista de todos los comentarios
-     *  en representación detailed
-     * @return  lista
+     * Obtiene una lista de todos los comentarios en representación detailed
+     *
+     * @return lista
      */
     @GET
-    public List<ComentarioDetailDTO> getComentarios()
-    {
+    public List<ComentarioDetailDTO> getComentarios() {
         return listEntityToDetailDTO(logic.getComentarios());
     }
-    
+
+    @GET
+    @Path("restaurantes/{id: [a-zA-Z0-9][a-zA-Z0-9]*}")
+    public List<ComentarioDetailDTO> getComentariosRestaurantes(@PathParam("id") String id) throws BusinessLogicException {
+        
+        ArrayList<ComentarioEntity> listaComentarios = new ArrayList<ComentarioEntity>();
+        RestauranteEntity restaurante = restLogic.getRestaurante(id);
+        for(SucursalEntity sucur : restaurante.getSucursales())
+        {
+            for(ComentarioEntity actual : sucur.getComentarios())
+            {
+                listaComentarios.add(actual);
+            }
+        }
+        
+        return listEntityToDetailDTO(listaComentarios);
+    }
+
     /**
-     * Retorna el comentario con el id especificado 
-     * Si el comentario con el id especificado no existe,
-     * se le notifica al usuario
+     * Retorna el comentario con el id especificado Si el comentario con el id
+     * especificado no existe, se le notifica al usuario
+     *
      * @param id
      * @return comentario
-     * @throws co.edu.uniandes.csw.puntosfidelidad.exceptions.BusinessLogicException 
+     * @throws
+     * co.edu.uniandes.csw.puntosfidelidad.exceptions.BusinessLogicException
      */
-    
     @GET
     @Path("{id: \\d+}")
-    public ComentarioDetailDTO getComentario(@PathParam("id") Long id) throws BusinessLogicException
-    {
-        try
-        {
-            return new ComentarioDetailDTO (logic.getComentario(id));
-        }
-        catch (Exception e)
-        {
+    public ComentarioDetailDTO getComentario(@PathParam("id") Long id) throws BusinessLogicException {
+        try {
+            return new ComentarioDetailDTO(logic.getComentario(id));
+        } catch (Exception e) {
             throw new WebApplicationException("El comentario con el id " + id + " no existe" + e, 404);
-        }       
+        }
     }
-    
+
     /**
-     * Actualiza un comentario a partir del DTO dado
-     * por parámetro, retorna el nuevo estado del comentario
+     * Actualiza un comentario a partir del DTO dado por parámetro, retorna el
+     * nuevo estado del comentario
+     *
      * @param id
      * @param nuevo
      * @return comentario
      */
     @PUT
     @Path("{id: \\d+}")
-    public ComentarioDetailDTO putComentario(@PathParam("id") Long id, ComentarioDTO nuevo)
-    {
-        try
-        {
+    public ComentarioDetailDTO putComentario(@PathParam("id") Long id, ComentarioDTO nuevo) {
+        try {
             logic.getComentario(id);
-        }
-        catch (NullPointerException e)
-        {
-            
+        } catch (NullPointerException e) {
+
             throw new WebApplicationException("El comentario con el id " + id + " no existe", 404);
-        } 
+        }
         return new ComentarioDetailDTO(logic.updateComentario(nuevo.toEntity()));
     }
-    
+
     //Los demás métodos rest están desde clases que las contienen (toca crearlas desde allá)
     //Como cliente y/o sucursal [ver api]
-    
     /**
-     * A partir de una lista de entidades, retorna una lista de DTO's
-     * con la información de las entidades que entraron por parámetro
+     * A partir de una lista de entidades, retorna una lista de DTO's con la
+     * información de las entidades que entraron por parámetro
      */
-    
-    private List<ComentarioDetailDTO> listEntityToDetailDTO(List<ComentarioEntity> entities)
-    {
+    private List<ComentarioDetailDTO> listEntityToDetailDTO(List<ComentarioEntity> entities) {
         List<ComentarioDetailDTO> dTOList = new ArrayList<>();
-        
-        for(ComentarioEntity entity: entities)
-        {
+
+        for (ComentarioEntity entity : entities) {
             //Convierto el entity con el constructor del DTO
             dTOList.add(new ComentarioDetailDTO(entity));
         }
-        
+
         return dTOList;
     }
 }
