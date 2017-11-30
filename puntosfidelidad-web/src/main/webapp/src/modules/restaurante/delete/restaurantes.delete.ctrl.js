abd2bc37f9cf345c5e71f7b5afea27ec596d101e
Sebastian Cespedes Otalora <s.cespedes10@sis.virtual.uniandes.edu.co>
2017-11-30 15:58:48 -0500

    Restaurante Crud desde Admin

--- puntosfidelidad-web/src/main/webapp/src/modules/restaurante/delete/restaurantes.delete.ctrl.js
+++ puntosfidelidad-web/src/main/webapp/src/modules/restaurante/delete/restaurantes.delete.ctrl.js
@@ -3,12 +3,36 @@
     mod.controller('restauranteDeleteCtrl', ['$scope', '$http', '$state',
         function ($scope, $http, $state) { 
             
+            
+            
             $scope.deleteRestaurante = function () {
                 $http.delete("http://localhost:8080/puntosfidelidad-web/api/restaurantes/"+ $state.params.restauranteNit , {}).then(function () {
                     $state.go('restaurantesList', {reload: true});
                 });
+                
+                
+                
+                
+            };
+            
+            
+            $scope.updateRestaurante = function () {
+                $http.put("api/restaurantes/" + $state.params.restauranteNit, {
+                    nit: "Anonimo",
+                    nombre: "anonimo",
+                    tipoComida:"anonimo",
+                 
+                }).then(function (response) {                    
+                    $state.go('restauranteAdministradorList', {restauranteNit: response.data.usuario}, {reload: true});
+                });
             };
         }
+        
+     
+        
+        
+        
+        
     ]);
 }
 )(window.angular);
