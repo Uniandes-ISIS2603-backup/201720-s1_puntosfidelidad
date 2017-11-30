591b54d559f4afc95d4d783fac424b2f92f8a555
Julián Manrique <ja.manrique@uniandes.edu.co>
2017-11-30 05:46:07 -0500

    comentarios list & look & feel & vive100 & 5:46am

--- puntosfidelidad-web/src/main/webapp/src/modules/comentario/comentarios.list.param.ctrl.js
+++ puntosfidelidad-web/src/main/webapp/src/modules/comentario/comentarios.list.param.ctrl.js
@@ -1,11 +1,40 @@
 (function (ng) {
     var mod = ng.module("comentariosNuevosModule");
     mod.controller("comentariosNuevosCtrl", ['$scope', '$http', function ($scope, $http) {
+            
+            $scope.restauranteActualStr = '';
+            $scope.comentarios = [];
             $scope.restaurantes = [];
             $http.get("http://localhost:8080/puntosfidelidad-web/api/restaurantes")
                     .then(function (response) {
                         $scope.restaurantes = response.data;
-            });
+            });           
+            
+           
+            if($scope.restauranteActualStr != '')
+            {
+                $http.get("http://localhost:8080/puntosfidelidad-web/api/comentarios/restaurantes/" + $scope.restauranteActualStr)
+                .then(function (response) {
+                    $scope.comentarios = response.data;
+                });
+
+                console.log($scope.comentarios);
+            }
+            
+            $scope.restauranteActual = function(restAct)
+            {
+                if(restAct != '')
+                {
+                    $http.get("http://localhost:8080/puntosfidelidad-web/api/comentarios/restaurantes/" + restAct)
+                    .then(function (response) {
+                        $scope.comentarios = response.data;
+                    });    
+                    console.log($scope.comentarios);
+                }
+            }
+
+            $scope.restauranteActual($scope.restauranteActualStr);
+                
 
             $scope.asignarEstrellasCSS = function(calificacion)
             {
